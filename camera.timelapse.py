17743a3c25c362c243664a308ad6ef2189d9c294
eat-sleep-code/ <info@eat-sleep-code.com>
2020-08-18 16:57:02 -0700

    Update camera.timelapse.py

--- camera.timelapse.py
+++ camera.timelapse.py
@@ -2,7 +2,10 @@ from picamera import PiCamera
 import ffmpeg
 import argparse
 import datetime
+import keyboard
+import os
 import subprocess
+import sys
 import threading
 import time
 
@@ -22,7 +25,7 @@ parser.add_argument('--outputFolder', dest='outputFolder', help='Set the folder 
 args = parser.parse_args()
 
 
-interval = args.interval or 60
+interval = args.interval or 15
 try:
 	interval = int(interval)
 except:
@@ -37,7 +40,7 @@ except:
 
 	
 renderVideo = args.renderVideo or True
-if renderVideo != False
+if renderVideo != False:
 	renderVideo = True
 
 	
@@ -67,7 +70,7 @@ def getFileName(imageCounter = 1):
 
 # ------------------------------------------------------------------------------
 
-def getFilePath(imageCount = 1):
+def getFilePath(imageCounter = 1):
 	try:
 		os.makedirs(outputFolder, exist_ok = True)
 	except OSError:
@@ -79,51 +82,65 @@ def getFilePath(imageCount = 1):
 
 # ------------------------------------------------------------------------------
 
-def captureTimelapse:
-	global interval
-	global outputFolder
-	for filename in camera.capture_continuous(getFilePath({counter})):	
-		time.sleep(interval) #seconds
+def captureTimelapse():
+	try:
+		global interval
+		global outputFolder
+		for filename in camera.capture_continuous(getFilePath('{counter:08d}')):	
+			time.sleep(interval) #seconds
+	except: 
+		print(' ERROR: Could not capture image. ')
 
 # ------------------------------------------------------------------------------
 
 def convertSequenceToVideo(dateToConvert):
-	global framerate
-	global outputFolder
-	dateToConvertStamp = dateToConvert.strftime("%Y%m%d")
-	output_options = {
-		'crf': 20,
-		'preset': 'slower',
-		'movflags': 'faststart',
-		'pix_fmt': 'yuv420p'
-	}
-	ffmpeg
-	.input(outputFolder + dateToConvertStamp +'-*.jpg', pattern_type='glob', framerate=framerate)
-	.filter_('deflicker', mode='pm', size=10)
-	.filter_('scale', size='hd1080', force_original_aspect_ratio='increase')
-	.output(dateToConvertStamp + '.mp4', **output_options)
-	.run()	
-
+	try:
+		global framerate
+		global outputFolder
+		dateToConvertStamp = dateToConvert.strftime("%Y%m%d")
+		output_options = {
+			'crf': 20,
+			'preset': 'slower',
+			'movflags': 'faststart',
+			'pix_fmt': 'yuv420p'
+		}
+		ffmpeg.input(outputFolder + dateToConvertStamp +'-*.jpg', pattern_type='glob', framerate=framerate).filter_('deflicker', mode='pm', size=10).filter_('scale', size='hd1080', force_original_aspect_ratio='increase').output(dateToConvertStamp + '.mp4', **output_options).run()	
+	except:
+		print(' ERROR: Could not convert sequence to video. ')
 
 # === Timelapse Capture ========================================================
 
 try: 
+	while True:
 
-	camera.start_preview(fullscreen=False, resolution=(1920, 1080), window=(0, 0, 1920, 1080))
-	time.sleep(2)	
+		if keyboard.is_pressed('ctrl+c') or keyboard.is_pressed('esc'):
+			# clear()
+			echoOn()
+			break
 
-	captureThread = threading.Thread(captureTimelapse, args=(,))
-	captureThread.start()
+		camera.start_preview(fullscreen=False, resolution=(1920, 1080), window=(0, 0, 640, 360))
+		time.sleep(2)	
 
-	while renderVideo:
-		yesterday = (datetime.date.today() - datetime.timedelta(days = 1))
-		yesterdayStamp = yesterday.strftime("%Y%m%d")
-		if exists(outputFolder + yesterdayStamp + ".mp4") == False:
-			convertThread = threading.Thread(convertSequenceToVideo, args=(yesterday,)
-			convertThread.start()
-		time.sleep(3600)
+		captureThread = threading.Thread(target=captureTimelapse)
+		captureThread.start()
+
+		while renderVideo:
+			yesterday = (datetime.date.today() - datetime.timedelta(days = 1))
+			yesterdayStamp = yesterday.strftime("%Y%m%d")
+			firstFrameExists = os.path.exists(outputFolder + yesterdayStamp + '-00000001.jpg')
+			videoExists = os.path.exists(outputFolder + yesterdayStamp + '.mp4')
+			#print(videoExists)
+			if firstFrameExists == True and videoExists == False:
+				convertThread = threading.Thread(target=convertSequenceToVideo, args=(yesterday,))
+				convertThread.start()
+			time.sleep(3600)
 
 
 except KeyboardInterrupt:
+	camera.close()
 	echoOn()
 	sys.exit(1)
+
+else:
+	echoOn()
+	sys.exit(0)
