ed2810fb91d5812e7f3596c7e696b0aa518f5928
eat-sleep-code/ <info@eat-sleep-code.com>
2020-08-20 22:24:31 -0700

    Update install-camera.timelapse.sh

--- install-camera.timelapse.sh
+++ install-camera.timelapse.sh
@@ -1,7 +1,7 @@
 # This script will install the camera and any required prerequisites.
 cd ~
 echo -e ''
-echo -e '\033[32mCamera [Installation Script] \033[0m'
+echo -e '\033[32mCamera (Timelapse) [Installation Script] \033[0m'
 echo -e '\033[32m-------------------------------------------------------------------------- \033[0m'
 echo -e ''
 echo -e '\033[93mUpdating package repositories... \033[0m'
@@ -10,10 +10,10 @@ sudo apt update
 echo ''
 echo -e '\033[93mInstalling prerequisites... \033[0m'
 sudo apt install -y git python3 python3-pip python3-picamera ffmpeg
-sudo pip3 install ffmpeg_python keyboard
+sudo pip3 install keyboard
 
 echo ''
-echo -e '\033[93mInstalling Camera... \033[0m'
+echo -e '\033[93mInstalling Camera (Timelapse)... \033[0m'
 cd ~
 sudo rm -Rf ~/camera.timelapse
 sudo git clone https://github.com/eat-sleep-code/camera.timelapse
