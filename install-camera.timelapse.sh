b5d743a1bf5becb58665b13317166032c5efc66e
eat-sleep-code/ <info@eat-sleep-code.com>
2020-08-21 14:15:38 -0700

    Add steps to install the automated upload script

--- install-camera.timelapse.sh
+++ install-camera.timelapse.sh
@@ -1,7 +1,7 @@
 # This script will install the camera and any required prerequisites.
 cd ~
 echo -e ''
-echo -e '\033[32mCamera (Timelapse) [Installation Script] \033[0m'
+echo -e '\033[32mCamera [Installation Script] \033[0m'
 echo -e '\033[32m-------------------------------------------------------------------------- \033[0m'
 echo -e ''
 echo -e '\033[93mUpdating package repositories... \033[0m'
@@ -10,16 +10,17 @@ sudo apt update
 echo ''
 echo -e '\033[93mInstalling prerequisites... \033[0m'
 sudo apt install -y git python3 python3-pip python3-picamera ffmpeg
-sudo pip3 install keyboard
+sudo pip3 install ffmpeg-python google-api-python-client
 
 echo ''
-echo -e '\033[93mInstalling Camera (Timelapse)... \033[0m'
+echo -e '\033[93mInstalling Camera... \033[0m'
 cd ~
 sudo rm -Rf ~/camera.timelapse
 sudo git clone https://github.com/eat-sleep-code/camera.timelapse
 sudo chown -R $USER:$USER camera.timelapse
 cd camera.timelapse
 sudo chmod +x camera.timelapse.py
+sudo chmod +x camera.timelapse.upload.py
 
 cd ~
 echo ''
@@ -27,7 +28,9 @@ echo -e '\033[93mSetting up alias... \033[0m'
 sudo sed -i '/\b\(function camera.timelapse\)\b/d' ~/.bash_aliases
 sudo sed -i '$ a function camera.timelapse { sudo python3 ~/camera.timelapse/camera.timelapse.py $@; }' ~/.bash_aliases
 echo -e 'You may use \e[1mcamera.timelapse <options>\e[0m to launch the program.'
-
+echo ''
+echo 'To use the automatic YouTube upload feature, you will need to update the config.json.'
+echo 'Please see the README file for more information.'
 echo ''
 echo -e '\033[32m-------------------------------------------------------------------------- \033[0m'
 echo -e '\033[32mInstallation completed. \033[0m'
