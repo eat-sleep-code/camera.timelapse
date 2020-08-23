3e914529ea89189b5e18f76e5287533f2f1620ac
eat-sleep-code/ <info@eat-sleep-code.com>
2020-08-22 19:37:26 -0700

    Update camera.timelapse.upload.py

--- camera.timelapse.upload.py
+++ camera.timelapse.upload.py
@@ -3,30 +3,29 @@ from apiclient.errors import HttpError
 from apiclient.http import MediaFileUpload
 from oauth2client.client import flow_from_clientsecrets
 from oauth2client.file import Storage
-from oauth2client.tools import argparser, run_flow
-import httplib
+from oauth2client.tools import run_flow, argparser
+import http.client
 import httplib2
 import keyboard
 import os
 import random
 import sys
+import subprocess
 import time
 
-version = "2020.08.21"
+version = "2020.08.22"
 
 validPrivacyStatus = ("public", "private", "unlisted")
 
 # === Argument Handling ========================================================
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--file", required=True, help="Select the file to upload")
-parser.add_argument("--title", default="Timelapse" help="Set the video title")
-parser.add_argument("--description", default="Timelapse Video", help="Set the video description")
-parser.add_argument("--category", default="22", help="Set the video category.  [ https://developers.google.com/youtube/v3/docs/videoCategories/list ]")
-parser.add_argument("--keywords", default="timelapse", help="Set the keywords (comma-seperated)")
-parser.add_argument("--privacyStatus", choices=validPrivacyStatus, default=validPrivacyStatus[0], help="Video privacy status.")
-parser.add_argument("--channel", help="Set the channel ID")
-args = parser.parse_args()
+argparser.add_argument("--file", required=True, help="Select the file to upload")
+argparser.add_argument("--title", default="Timelapse", help="Set the video title")
+argparser.add_argument("--description", default="Timelapse Video", help="Set the video description")
+argparser.add_argument("--category", default="22", help="Set the video category.  [ https://developers.google.com/youtube/v3/docs/videoCategories/list ]")
+argparser.add_argument("--keywords", default="timelapse", help="Set the keywords (comma-seperated)")
+argparser.add_argument("--privacyStatus", choices=validPrivacyStatus, default=validPrivacyStatus[0], help="Video privacy status.")
+args = argparser.parse_args()
 
 if not os.path.exists(args.file):
 	exit("Please specify a valid file using the --file= parameter.")
@@ -35,12 +34,12 @@ if not os.path.exists(args.file):
 
 httplib2.RETRIES = 1
 maxRetries = 10
-retryExceptions = (httplib2.HttpLib2Error, IOError, httplib.NotConnected, httplib.IncompleteRead, httplib.ImproperConnectionState, httplib.CannotSendRequest, httplib.CannotSendHeader, httplib.ResponseNotReady, httplib.BadStatusLine)
+retryExceptions = (httplib2.HttpLib2Error, IOError, http.client.NotConnected, http.client.IncompleteRead, http.client.ImproperConnectionState, http.client.CannotSendRequest, http.client.CannotSendHeader, http.client.ResponseNotReady, http.client.BadStatusLine)
 retryStatusCodes = [500, 502, 503, 504]
 
 # ------------------------------------------------------------------------------
 
-configFile= "config.json"
+configFile= "/home/pi/camera.timelapse/config.json"
 uploadScope = "https://www.googleapis.com/auth/youtube.upload"
 apiServiceName = "youtube"
 apiVersion = "v3"
@@ -75,11 +74,7 @@ def initalizeUpload(youtube, options):
 	tags = None
 	if options.keywords:
 		tags = options.keywords.split(",")
-	targetChannel = youtube.channels.list(part="id", mine=true)[0]
-	if options.channel:
-		if options.channel != 'DEFAULT':
-			targetChannel = options.channel
-	body=dict(snippet=dict(title=options.title, description=options.description, tags=tags, categoryId=options.category, channelId=targetChannel),status=dict(privacyStatus=options.privacyStatus))
+	body=dict(snippet=dict(title=options.title, description=options.description, tags=tags, categoryId=options.category) ,status=dict(privacyStatus=options.privacyStatus))
 	uploadRequest = youtube.videos().insert(part=",".join(body.keys()),body=body,media_body=MediaFileUpload(options.file, chunksize=-1, resumable=True))
 	resumableUpload(uploadRequest)
 
@@ -92,29 +87,31 @@ def resumableUpload(uploadRequest):
 	while response is None:
 		try:
 			print("Uploading file...")
-      		status, response = uploadRequest.next_chunk()
+			status, response = uploadRequest.next_chunk()
 			if response is not None:
 				if 'id' in response:
-					print("Video id '%s' was successfully uploaded." % response['id'])
+					print('Video id ' + str(response['id']) + ' was successfully uploaded.')
+					echoOn()
+					sys.exit(0)
 				else:
- 					exit("The upload failed with an unexpected response: %s" % response)
-		except HttpError, e:
- 			if e.resp.status in retryStatusCodes:
-				error = "A retriable HTTP error %d occurred:\n%s" % (e.resp.status, e.content)
+ 					exit('ERROR: The upload failed with an unexpected response of ' + str(response))
+		except HttpError as ex:
+			if ex.resp.status in retryStatusCodes:
+				error = 'INFO: A retriable HTTP error ' + str(ex.resp.status) + ' occurred: ' + str(ex.content)
 			else:
 				raise
- 		except retryExceptions, e:
-			error = "A retriable error occurred: %s" % e
+		except retryExceptions as ex:
+			error = 'A retriable error occurred: ' + str(ex)
 
- 		if error is not None:
- 			print(error)
+		if error is not None:
+			print(error)
 			retryAttempt += 1
 			if retryAttempt > maxRetries:
 				exit('Maximum retries exceeded.')
 
 			maxSleep = 2 ** retry
 			sleepTime = random.random() * maxSleep
-			print("Sleeping %f seconds and then retrying..." % sleepTime)
+			print('Sleeping ' + sleepTime + ' and then retrying...')
 			time.sleep(sleepTime)
 
 
@@ -124,13 +121,12 @@ try:
 	os.chdir('/home/pi') 
 	while True:
 		youtube = getAuthenticatedService(args)
-  		try:
-    		initalizeUpload(youtube, args)
-  		except HttpError, e:
-			print "An HTTP error %d occurred:\n%s" % (e.resp.status, e.content)
+		try:
+			initalizeUpload(youtube, args)
+		except HttpError as ex:
+			print('ERROR: An HTTP error ' + str(ex.resp.status) + ' occurred: ' + str(ex.content))
 
 except KeyboardInterrupt:
-	camera.close()
 	echoOn()
 	sys.exit(1)
 
