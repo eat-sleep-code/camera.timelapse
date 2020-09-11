2145c9e52a0c1f49dd93a889ab53eab879f5e052
eat-sleep-code/ <info@eat-sleep-code.com>
2020-09-11 16:41:25 -0700

    Add supporting libraries for last image analysis

--- install-camera.timelapse.sh
+++ install-camera.timelapse.sh
@@ -10,7 +10,7 @@ sudo apt update
 echo ''
 echo -e '\033[93mInstalling prerequisites... \033[0m'
 sudo apt install -y git python3 python3-pip python3-picamera ffmpeg
-sudo pip3 install keyboard google-api-python-client oauth2client 
+sudo pip3 install keyboard google-api-python-client oauth2client numpy Pillow
 
 echo ''
 echo -e '\033[93mInstalling Camera... \033[0m'
