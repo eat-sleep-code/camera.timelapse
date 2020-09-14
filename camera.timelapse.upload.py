efb07dac970ec94d13c815923540964631ca342a
eat-sleep-code/ <info@eat-sleep-code.com>
2020-09-14 14:03:28 -0700

    Update camera.timelapse.upload.py

--- camera.timelapse.upload.py
+++ camera.timelapse.upload.py
@@ -13,22 +13,22 @@ import sys
 import subprocess
 import time
 
-version = "2020.08.22"
+version = '2020.09.14'
 
-validPrivacyStatus = ("public", "private", "unlisted")
+validPrivacyStatus = ('public', 'private', 'unlisted')
 
 # === Argument Handling ========================================================
 print(sys.argv)
-argparser.add_argument("--file", required=True, help="Select the file to upload")
-argparser.add_argument("--title", default="Timelapse", help="Set the video title")
-argparser.add_argument("--description", default="Timelapse Video", help="Set the video description")
-argparser.add_argument("--category", default="22", help="Set the video category.  [ https://developers.google.com/youtube/v3/docs/videoCategories/list ]")
-argparser.add_argument("--keywords", default="timelapse", help="Set the keywords (comma-seperated)")
-argparser.add_argument("--privacyStatus", choices=validPrivacyStatus, default=validPrivacyStatus[0], help="Video privacy status.")
+argparser.add_argument('--file', required=True, help='Select the file to upload')
+argparser.add_argument('--title', default='Timelapse', help='Set the video title', type=str)
+argparser.add_argument('--description', default='Timelapse Video', help='Set the video description', type=str)
+argparser.add_argument('--category', default='22', help='Set the video category.  [ https://developers.google.com/youtube/v3/docs/videoCategories/list ]', type=int)
+argparser.add_argument('--keywords', default='timelapse', help='Set the keywords (comma-seperated)', type=str)
+argparser.add_argument('--privacyStatus', choices=validPrivacyStatus, default=validPrivacyStatus[0], help='Video privacy status.', type=str)
 args = argparser.parse_args()
 
 if not os.path.exists(args.file):
-	exit("Please specify a valid file using the --file= parameter.")
+	exit('Please specify a valid file using the --file= parameter.')
 
 # ------------------------------------------------------------------------------
 
@@ -39,10 +39,10 @@ retryStatusCodes = [500, 502, 503, 504]
 
 # ------------------------------------------------------------------------------
 
-configFile= "/home/pi/camera.timelapse/config.json"
-uploadScope = "https://www.googleapis.com/auth/youtube.upload"
-apiServiceName = "youtube"
-apiVersion = "v3"
+configFile= '/home/pi/camera.timelapse/config.json'
+uploadScope = 'https://www.googleapis.com/auth/youtube.upload'
+apiServiceName = 'youtube'
+apiVersion = 'v3'
 
 
 # === Echo Control =============================================================
@@ -51,16 +51,14 @@ def echoOff():
 	subprocess.run(['stty', '-echo'], check=True)
 def echoOn():
 	subprocess.run(['stty', 'echo'], check=True)
-def clear():
-	subprocess.call('clear' if os.name == 'posix' else 'cls')
-clear()
+
 
 
 # === Functions ================================================================
 
 def getAuthenticatedService(args):
-	flow = flow_from_clientsecrets(configFile, scope=uploadScope, message="Please edit your config.json file.")
-	storage = Storage("%s-oauth2.json" % sys.argv[0])
+	flow = flow_from_clientsecrets(configFile, scope=uploadScope, message='Please edit your config.json file.')
+	storage = Storage('%s-oauth2.json' % sys.argv[0])
 	credentials = storage.get()
 
 	if credentials is None or credentials.invalid:
@@ -73,9 +71,9 @@ def getAuthenticatedService(args):
 def initalizeUpload(youtube, options):
 	tags = None
 	if options.keywords:
-		tags = options.keywords.split(",")
+		tags = options.keywords.split(',')
 	body=dict(snippet=dict(title=options.title, description=options.description, tags=tags, categoryId=options.category) ,status=dict(privacyStatus=options.privacyStatus))
-	uploadRequest = youtube.videos().insert(part=",".join(body.keys()),body=body,media_body=MediaFileUpload(options.file, chunksize=-1, resumable=True))
+	uploadRequest = youtube.videos().insert(part=','.join(body.keys()),body=body,media_body=MediaFileUpload(options.file, chunksize=-1, resumable=True))
 	resumableUpload(uploadRequest)
 
 # ------------------------------------------------------------------------------
@@ -86,7 +84,6 @@ def resumableUpload(uploadRequest):
 	retryAttempt = 0
 	while response is None:
 		try:
-			print("Uploading file...")
 			status, response = uploadRequest.next_chunk()
 			if response is not None:
 				if 'id' in response:
